2feecb7895232b1be0179809666e92b8b060841e
LavresJ <86180798+LavresJ@users.noreply.github.com>
2021-12-29 21:50:34 +0800

    Add files via upload

---
+++ Owl my class 網頁/js_files/general.js
@@ -0,0 +1,26 @@
+var courseInfo = {
+		name: "系統分析與設計",
+		semester: "110-1",
+		code: "IM3069*",
+		division: "資管系",
+		professor: "喵喵喵",
+		credits: 3,
+		description: "課程介紹課程介紹課程介紹課程介紹課程介紹課程介紹課程介紹課程介紹課程介紹"
+};
+$(".course-name").text(courseInfo.name);
+$(".course-semester").text(courseInfo.semester);
+$(".course-code").text(courseInfo.code);
+$(".course-division").text(courseInfo.division);
+$(".course-professor").text(courseInfo.professor);
+$(".course-credits").text(courseInfo.credits);
+$(".course-description").text(courseInfo.description);
+
+////////////判斷是教授還是學生/////////
+var userIsProfessor = 0;
+if(userIsProfessor){
+	$(".stu-only").remove();
+}else{
+	$(".pro-only").remove();
+}
+
+
