9feb945aa082693dd4fee84bb13aaf4df9443462
andy258741 <75974594+andy258741@users.noreply.github.com>
2022-01-14 00:41:12 +0800

    Add files via upload

---
+++ WEB-INF/classes/ncu/im3069/demo/app/Student.java
@@ -0,0 +1,166 @@
+package sa_project.app;
+
+import org.json.*;
+
+// TODO: Auto-generated Javadoc
+/**
+ * <p>
+ * The Class Member
+ * Member類別（class）具有會員所需要之屬性與方法，並且儲存與會員相關之商業判斷邏輯<br>
+ * </p>
+ * 
+ * @author IPLab
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+
+public class Student extends User{
+    
+	private int CourseListId;
+	private int Grade;
+    /** mh，MemberHelper之物件與Member相關之資料庫方法（Sigleton） */
+    private StudentHelper sh =  StudentHelper.getHelper();
+    
+    /**
+     * 實例化（Instantiates）一個新的（new）Member物件<br>
+     * 採用多載（overload）方法進行，此建構子用於建立會員資料時，產生一名新的會員
+     *
+     * @param email 會員電子信箱
+     * @param password 會員密碼
+     * @param name 會員姓名
+     */
+    public Student(String email, String password, String name, int grade) {
+        this.Email = email;
+        this.Password = password;
+        this.Name = name;
+        this.Grade = grade;
+        update();
+    }
+
+    /**
+     * 實例化（Instantiates）一個新的（new）Member物件<br>
+     * 採用多載（overload）方法進行，此建構子用於更新會員資料時，產生一名會員同時需要去資料庫檢索原有更新時間分鐘數與會員組別
+     * 
+     * @param id 會員編號
+     * @param email 會員電子信箱
+     * @param password 會員密碼
+     * @param name 會員姓名
+     */
+    public Student(int id, String email, String password, String name) {
+        this.Id = id;
+        this.Email = email;
+        this.Password = password;
+        this.Name = name;
+    }
+    
+    /**
+     * 實例化（Instantiates）一個新的（new）Member物件<br>
+     * 採用多載（overload）方法進行，此建構子用於查詢會員資料時，將每一筆資料新增為一個會員物件
+     *
+     * @param id 會員編號
+     * @param email 會員電子信箱
+     * @param password 會員密碼
+     * @param name 會員姓名
+     * @param login_times 更新時間的分鐘數
+     * @param status the 會員之組別
+     */
+    public Student(int id, String email, String password, String name, String department, int grade) {
+        this.Id = id;
+        this.Email = email;
+        this.Password = password;
+        this.Name = name;
+        this.Department = department;
+        this.Grade = grade;
+    }
+    
+    /**
+     * 取得會員之編號
+     *
+     * @return the id 回傳會員編號
+     */
+    public int getId() {
+        return this.Id;
+    }
+
+    /**
+     * 取得會員之電子郵件信箱
+     *
+     * @return the email 回傳會員電子郵件信箱
+     */
+    public String getEmail() {
+        return this.Email;
+    }
+    
+    /**
+     * 取得會員之姓名
+     *
+     * @return the name 回傳會員姓名
+     */
+    public String getName() {
+        return this.Name;
+    }
+
+    /**
+     * 取得會員之密碼
+     *
+     * @return the password 回傳會員密碼
+     */
+    public String getPassword() {
+        return this.Password;
+    }
+    
+    /**
+     * 取得會員資料之會員組別
+     *
+     * @return the status 回傳會員組別
+     */
+    public String getDepartment() {
+        return this.Department;
+    }
+    
+    public int getCourseListId() {
+        return this.CourseListId;
+    }
+    
+    public int getGrade() {
+    	return this.Grade;
+    }
+    
+    /**
+     * 更新會員資料
+     *
+     * @return the JSON object 回傳SQL更新之結果與相關封裝之資料
+     */
+    public JSONObject update() {
+        /** 新建一個JSONObject用以儲存更新後之資料 */
+        JSONObject data = new JSONObject();
+        /** 檢查該名會員是否已經在資料庫 */
+        if(this.Id != 0) {
+            /** 透過MemberHelper物件，更新目前之會員資料置資料庫中 */
+            data = sh.update(this);
+        }
+        
+        return data;
+    }
+    
+    /**
+     * 取得該名會員所有資料
+     *
+     * @return the data 取得該名會員之所有資料並封裝於JSONObject物件內
+     */
+    public JSONObject getData() {
+        /** 透過JSONObject將該名會員所需之資料全部進行封裝*/ 
+        JSONObject jso = new JSONObject();
+        jso.put("StudentId", getId());
+        jso.put("StudentName", getName());
+        jso.put("Email", getEmail());
+        jso.put("Password", getPassword());
+        jso.put("CourseListId", getCourseListId());
+        jso.put("Department", getDepartment());
+        jso.put("Grade", getGrade());
+        
+        return jso;
+    }
+    
+
+}
