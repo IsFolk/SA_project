4aacd491a67a7a213e3e57d0385f956a910f5808
j20015798 <72622577+j20015798@users.noreply.github.com>
2022-01-14 13:53:21 +0800

    Add files via upload

---
+++ WEB-INF/classes/ncu/im3069/demo/controller/HandInController.java
@@ -0,0 +1,200 @@
+package ncu.im3069.demo.controller;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import org.json.*;
+
+import ncu.im3069.demo.app.Test;
+import ncu.im3069.demo.app.TestHelper;
+import ncu.im3069.demo.app.Question;
+import ncu.im3069.demo.app.QuestionHelper;
+import ncu.im3069.demo.app.HandIn;
+import ncu.im3069.demo.app.HandInHelper;
+import ncu.im3069.tools.JsonReader;
+/**
+ * Servlet implementation class HandInController
+ */
+@WebServlet("/api/HandIn.do")
+
+
+
+public class HandInController extends HttpServlet {
+    
+    /** The Constant serialVersionUID. */
+    private static final long serialVersionUID = 1L;
+    
+    /** qh，QuestionHelper之物件與Member相關之資料庫方法（Sigleton） */
+    private HandInHelper hih =  HandInHelper.getHelper();
+   
+    /** th，TestHelper 之物件與 test 相關之資料庫方法（Sigleton） */
+	//private TestHelper th =  TestHelper.getHelper();
+
+    public HandInController() {
+        super();
+    }
+
+ 
+    /**
+     * 處理Http Method請求POST方法（新增資料）
+     *
+     * @param request Servlet請求之HttpServletRequest之Request物件（前端到後端）
+     * @param response Servlet回傳之HttpServletResponse之Response物件（後端到前端）
+     * @throws ServletException the servlet exception
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        /** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
+        JsonReader jsr = new JsonReader(request);
+        JSONObject jso = jsr.getObject();
+        
+        /** 取出經解析到JSONObject之Request參數 */
+        String ansA = jso.getString("HandInAnswerA");
+        String ansB = jso.getString("HandInAnswerB");
+        String ansC = jso.getString("HandInAnswerC");
+        String ansD = jso.getString("HandInAnswerD");
+
+        
+        
+        /** 建立一個新的會員物件 */
+        HandIn h = new HandIn(ansA, ansB, ansC, ansD); 
+        
+
+        /** 透過MemberHelper物件的checkDuplicate()檢查該會員電子郵件信箱是否有重複 */
+     //   else if (!ch.checkDuplicate(c)) {
+            /** 透過MemberHelper物件的create()方法新建一個會員至資料庫 */
+            JSONObject data = hih.create(h);
+            
+            /** 新建一個JSONObject用於將回傳之資料進行封裝 */
+            JSONObject resp = new JSONObject();
+            resp.put("status", "200");
+            resp.put("message", "成功! 新增一個回答...");
+            resp.put("response", data);
+            
+            /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+            jsr.response(resp, response);
+     //   }
+      //  else {
+      //      /** 以字串組出JSON格式之資料 */
+      //      String resp = "{\"status\": \'400\', \"message\": \'新增帳號失敗，此E-Mail帳號重複！\', \'response\': \'\'}";
+      //      /** 透過JsonReader物件回傳到前端（以字串方式） */
+      //      jsr.response(resp, response);
+      //  }
+    }
+
+    /**
+     * 處理Http Method請求GET方法（取得資料）
+     *
+     * @param request Servlet請求之HttpServletRequest之Request物件（前端到後端）
+     * @param response Servlet回傳之HttpServletResponse之Response物件（後端到前端）
+     * @throws ServletException the servlet exception
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        /** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
+        JsonReader jsr = new JsonReader(request);
+        /** 若直接透過前端AJAX之data以key=value之字串方式進行傳遞參數，可以直接由此方法取回資料 */
+        String id = jsr.getParameter("id");
+        
+        /** 判斷該字串是否存在，若存在代表要取回個別會員之資料，否則代表要取回全部資料庫內會員之資料 */
+     //   if (id.isEmpty()) {
+            /** 透過MemberHelper物件之getAll()方法取回所有會員之資料，回傳之資料為JSONObject物件 */
+            JSONObject query = hih.getAll();
+            
+            /** 新建一個JSONObject用於將回傳之資料進行封裝 */
+            JSONObject resp = new JSONObject();
+            resp.put("status", "200");
+            resp.put("message", "所有題目資料取得成功");
+            resp.put("response", query);
+    
+            /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+            jsr.response(resp, response);
+      //  }
+        
+    //    else {
+            /** 透過MemberHelper物件的getByID()方法自資料庫取回該名會員之資料，回傳之資料為JSONObject物件 */
+   //         JSONObject query = hih.getByID(id);
+            
+            
+            /** 新建一個JSONObject用於將回傳之資料進行封裝 */
+     //       JSONObject resp = new JSONObject();
+     //       resp.put("status", "200");
+     //       resp.put("message", "答題資料取得成功");
+     //       resp.put("response", query);
+    
+            /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+     //       jsr.response(resp, response);
+     //  }       
+    				
+    }
+
+    /**
+     * 處理Http Method請求DELETE方法（刪除）
+     *
+     * @param request Servlet請求之HttpServletRequest之Request物件（前端到後端）
+     * @param response Servlet回傳之HttpServletResponse之Response物件（後端到前端）
+     * @throws ServletException the servlet exception
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    public void doDelete(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        /** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
+        JsonReader jsr = new JsonReader(request);
+        JSONObject jso = jsr.getObject();
+        
+        /** 取出經解析到JSONObject之Request參數 */
+        int id = jso.getInt("id");
+        
+        /** 透過MemberHelper物件的deleteByID()方法至資料庫刪除該名會員，回傳之資料為JSONObject物件 */
+        JSONObject query = hih.deleteByID(id);
+        
+        /** 新建一個JSONObject用於將回傳之資料進行封裝 */
+        JSONObject resp = new JSONObject();
+        resp.put("status", "200");
+        resp.put("message", "題目移除成功！");
+        resp.put("response", query);
+
+        /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+        jsr.response(resp, response);
+    }
+
+    /**
+     * 處理Http Method請求PUT方法（更新）
+     *
+     * @param request Servlet請求之HttpServletRequest之Request物件（前端到後端）
+     * @param response Servlet回傳之HttpServletResponse之Response物件（後端到前端）
+     * @throws ServletException the servlet exception
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    public void doPut(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        /** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
+        JsonReader jsr = new JsonReader(request);
+        JSONObject jso = jsr.getObject();
+        
+        /** 取出經解析到JSONObject之Request參數 */
+        int id = jso.getInt("id");
+        String answer1 = jso.getString("handInAns1");
+        String answer2 = jso.getString("handInAns2");
+        String answer3 = jso.getString("handInAns3");
+        String answer4 = jso.getString("handInAns4");
+
+        /** 透過傳入之參數，新建一個以這些參數之會員Question物件 */
+        HandIn h = new HandIn(id, answer1, answer2, answer3, answer4);
+        
+        /** 透過Member物件的update()方法至資料庫更新該名會員資料，回傳之資料為JSONObject物件 */
+        JSONObject data = h.update();
+        hih.correctAnswer(h);
+        /** 新建一個JSONObject用於將回傳之資料進行封裝 */
+        JSONObject resp = new JSONObject();
+        resp.put("status", "200");
+        resp.put("message", "成功! 更新作答...");
+        resp.put("response", data);
+        
+        /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+        jsr.response(resp, response);
+    }
+}
